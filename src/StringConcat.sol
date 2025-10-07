7d97ae27056d6bd4eaf52bcd5dc777a5ac330cb0
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-10-07 11:44:58 +0300

    ✅ resolved StringConcat

--- src/StringConcat.sol
+++ src/StringConcat.sol
@@ -2,7 +2,16 @@
 pragma solidity 0.8.28;
 
 contract StringConcat {
-    function concat(string memory a, string memory b) public pure returns (string memory) {
+    function concat(
+        string memory a,
+        string memory b
+    ) public pure returns (string memory) {
         // return the concatenation of a and b
+
+        // we can have two different options for it
+        // option 1 is for solidity less than 0.8.12
+
+        // option 1:  return string(abi.encodePacked(a, b));
+        return string.concat(a, b); // option 2
     }
 }
