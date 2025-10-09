3f55208707cdb7204087ee34b3e774ceae79290c
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-10-09 23:48:42 +0300

    ✅ resolved NinetyNinePercent

--- src/NinetyNinePercent.sol
+++ src/NinetyNinePercent.sol
@@ -10,5 +10,7 @@ contract NinetyNinePercent {
         // example: x = 32999999999, y = 33333333333 return false
 
         // return true if x >= y * 0.99. But remember, Soliidty does not have floats.
+
+        return x * 100 >= y * 99;
     }
 }
