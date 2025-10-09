c248e329de33540ec29f4b957f7e31213ae4db9a
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-10-10 00:05:55 +0300

    ✅ resolve RationalFunction

--- src/RationalFunction.sol
+++ src/RationalFunction.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.28;
 
 contract RationalFunction {
-
     function f(uint256 x) public pure returns (uint256) {
         // return the function f(x) = 1 / (1 - 1/x) with two decimals rounded down
         // f(0) = revert
@@ -12,5 +11,6 @@ contract RationalFunction {
         // f(4) = 133
         // f(5) = 125
 
+        return (x * 100) / (x - 1);
     }
 }
