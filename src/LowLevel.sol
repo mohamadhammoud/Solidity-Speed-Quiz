1c24c6fdba1069ab25076a0c61196d89c741940e
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-10-22 02:47:47 +0300

    ✅ Resolve LowLevel

--- src/LowLevel.sol
+++ src/LowLevel.sol
@@ -7,8 +7,22 @@ contract LowLevel {
         // do not use an interface, use a low level call
         // return true if the call succeeded
         // return false if the call failed
-
         // bonus challenge: use an interface and a high level call to accomplish the same task
-
+        //------------------
+        // Solution 1
+        // bytes memory data = abi.encodeWithSignature("foo()");
+        // (bool success, ) = a.call(data);
+        // return success;
+        //------------------
+        // Solution2
+        try IFoo(a).foo() {
+            return true;
+        } catch {
+            return false;
+        }
     }
+}
+
+interface IFoo {
+    function foo() external;
 }
