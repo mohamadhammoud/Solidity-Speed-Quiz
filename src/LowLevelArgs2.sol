e63ad7022afb2be98c0b3931fec55fb4570a43ec
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-10-22 18:19:46 +0300

    ✅ Resolved LowLevelArgs2

--- src/LowLevelArgs2.sol
+++ src/LowLevelArgs2.sol
@@ -6,6 +6,23 @@ contract LowLevelArgs2 {
         // call rare(x, y) using a low-level call
         // if the low level call reverts, revert also
 
+        // Solution 1
+        // bytes memory data = abi.encodeWithSignature(
+        //     "rare(uint256,uint256)",
+        //     x,
+        //     y
+        // );
+
+        // (bool success, bytes memory returnedData) = a.call(data);
+        // require(success);
+
         // bonus challenge: use an interface and a high level call to accomplish the same task
+        // Solution 2
+        try IRare(a).rare(x, y) {} catch (bytes memory lowLevelError) {
+            revert();
+        }
     }
+}
+interface IRare {
+    function rare(uint256 x, uint256 y) external;
 }
