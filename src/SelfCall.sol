e958faf848810fcd8956578954bc0b76e8289d37
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-10-22 19:19:50 +0300

    ✅ Resolved SelfCall

--- src/SelfCall.sol
+++ src/SelfCall.sol
@@ -2,12 +2,15 @@
 pragma solidity 0.8.28;
 
 contract SelfCall {
-
     bool public success;
     function main() public {
         // call me() on this contract
         // think outside the box! You can only write code here, no creating external contracts
         // or modifying other parts of this contract
+
+        bytes memory data = abi.encodeWithSignature("me()");
+
+        address(this).call(data);
     }
 
     function me() external {
