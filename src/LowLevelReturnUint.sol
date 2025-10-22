3f9930039d50c95f0703107d5753d2868801c637
Mohamad Hammoud <mohamad.hammoudk@gmail.com>
2025-10-22 18:32:22 +0300

    ✅ Resolved LowLevelReturnUint

--- src/LowLevelReturnUint.sol
+++ src/LowLevelReturnUint.sol
@@ -8,13 +8,15 @@ contract LowLevelReturnUint {
         // return the return value of the call
 
         bytes memory data = abi.encodeWithSignature("bar()");
-
         (bool success, bytes memory returnedData) = a.call(data);
-
         uint256 num = abi.decode(returnedData, (uint256));
-
         return num;
 
         // bonus challenge: use an interface and a high level call to accomplish the same task
+        return IBar(a).bar();
     }
+}
+
+interface IBar {
+    function bar() external returns (uint);
 }
